ed19dda60e59983efb9baf765b20e13f51423ab5
tom1193 <thomas8289@gmail.com>
2017-11-19 16:49:45 -0500

    connected app to firebase project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDqshlzqz65hd51mRelSYdmZNcvQ1WrTQg",
+  authDomain: "test-e285e.firebaseapp.com",
+  databaseURL: "https://test-e285e.firebaseio.com",
+  projectId: "test-e285e",
+  storageBucket: "test-e285e.appspot.com",
+  messagingSenderId: "71945885230"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
