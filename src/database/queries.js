3d5f4a6dc1fda5cdd02fd4aa3057a68655e8f677
QKevCheng <qcheng16@gmail.com>
2017-12-10 16:21:05 -0500

    updated database

--- src/database/queries.js
+++ src/database/queries.js
@@ -1,6 +1,12 @@
 import database from './config';
 console.log(database)
 const userRef = database.collection("users")
+export function test(){
+     database.collection("stories").orderBy("popularity").limit(3)
+     .onSnapshot(dataSnapshot => {
+            console.log(dataSnapshot.docs);
+     });
+}
 
 //==========================User-Related Query===============================================
 export function getUserStory(uid){
@@ -32,9 +38,9 @@ export function topUsers(x){
   var ref = database.collection("users");
   var query = ref.orderBy("numStories").limit(x);
 
-  return ref.get().then(function(refSnapshot){
+  return query.get().then(function(querySnapshot){
       let res = []
-      refSnapshot.forEach(function(doc){
+      querySnapshot.forEach(function(doc){
           doc && doc.exists ? res.push(doc.data().username) : null
       })
       return Promise.resolve(res)
@@ -42,15 +48,32 @@ export function topUsers(x){
 
   }
 
+export function numStoriesIncrement(uid){
+    var ref = database.collection("users").doc(uid.toString());
+      getStoryNumber(uid).then(function(result){
+          return ref.update({
+              numStories: result + 1
+          })
+          .then(function() {
+              console.log("NumStories successfully incremented!");
+          })
+          .catch(function(error) {
+              // The document probably doesn't exist.
+              console.error("Error updating document: ", error);
+          });
+      })
+  }
+
+
 //==========================Story-Related Query===============================================
 
 export function getEntitiesInStory(uid, sid){
-    var ref = userRef.doc(uid.toString()).collection("stories").doc(sid).collection("entities");
+    var ref = userRef.doc(uid.toString()).collection("stories").doc(sid.toString()).collection("entities");
     var query = ref.orderBy("order");
 
-    return ref.get().then(function(refSnapshot){
+    return query.get().then(function(querySnapshot){
         let res = []
-        refSnapshot.forEach(function(doc){
+        querySnapshot.forEach(function(doc){
             doc && doc.exists ? res.push(doc.data().estring) : null
         })
         return Promise.resolve(res)
@@ -59,7 +82,7 @@ export function getEntitiesInStory(uid, sid){
 
 export function setPublic(uid,sid){
     var ref = userRef.doc(uid.toString()).collection("stories");
-    return ref.doc(sid).update({
+    return ref.doc(sid.toString()).update({
         privacy: false
     })
     .then(function() {
@@ -73,7 +96,7 @@ export function setPublic(uid,sid){
 
 export function setPrivate(uid,sid){
     var ref = userRef.doc(uid.toString()).collection("stories");
-    return ref.doc(sid).update({
+    return ref.doc(sid.toString()).update({
         privacy: true
     })
     .then(function() {
@@ -86,7 +109,7 @@ export function setPrivate(uid,sid){
 }
 
 export function getPop(sid){
-    var ref = database.collection("stories").doc(sid);
+    var ref = database.collection("stories").doc(sid.toString());
 
         return ref.get().then(function(doc) {
             if (doc && doc.exists) {
@@ -98,8 +121,10 @@ export function getPop(sid){
         })
 }
 
-export function upvoteStory(sid){
-    var ref = database.collection("stories").doc(sid);
+export function upvoteStory(uid, sid){
+    var ref = database.collection("stories").doc(sid.toString());
+    var UserStoryRef = userRef.doc(uid.toString()).collection("myStories").doc(sid.toString());
+
     getPop("s1").then(function(result){
         return ref.update({
             popularity: result + 1
@@ -112,10 +137,32 @@ export function upvoteStory(sid){
             console.error("Error updating document: ", error);
         });
     })
-    //getPop(sid).then(function(result){
-    //    console.log(result)
-    //})
 
+    getPop("s1").then(function(result){
+        return UserStoryRef.update({
+            popularity: result + 1
+        })
+        .then(function() {
+            console.log("Popularity successfully upvoted!");
+        })
+        .catch(function(error) {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+    })
+}
+
+export function topStories(x){
+  var ref = database.collection("stories");
+  var query = ref.orderBy("popularity").limit(x);
+
+  return query.get().then(function(querySnapshot){
+      let res = []
+      querySnapshot.forEach(function(doc){
+          doc && doc.exists ? res.push(doc.data().string) : null
+      })
+      return Promise.resolve(res)
+  })
 }
 
 
@@ -129,17 +176,33 @@ export function writeUserData(uid, username, numStories){
 }
 
 export function writeStoryData(uid, sid, timestamp, string, pop, privacy) {
-    database.collection("users").doc(uid.toString()).collection("stories").doc(sid).set({
+    database.collection("users").doc(uid.toString()).collection("myStories").doc(sid.toString()).set({
         sid: sid,
         timestamp: timestamp,
         string: string,
         popularity: pop,
         privacy: privacy
     });
+
+    database.collection("stories").doc(sid.toString()).set({
+        sid: sid,
+        uid: uid,
+        timestamp: timestamp,
+        string: string,
+        popularity: pop,
+        privacy: privacy
+    });
+
+    numStoriesIncrement(uid)
 }
 
 export function writeEntityInStoryData(uid, sid, estring, order, sentiment) {
-    database.collection("users").doc(uid.toString()).collection("stories").doc(sid).collection("entities").doc(estring).set({
+    database.collection("users").doc(uid.toString()).collection("myStories").doc(sid.toString()).collection("entities").doc(estring).set({
+        estring: estring,
+        order: order,
+        sentiment: sentiment //0=negative 1=neutral 2=positive
+    });
+    database.collection("stories").doc(sid.toString()).collection("entities").doc(estring).set({
         estring: estring,
         order: order,
         sentiment: sentiment //0=negative 1=neutral 2=positive
@@ -153,7 +216,7 @@ export function writeEntityData(name) {
 }
 
 export function writeImageData(iid, name, sentiment, url, numVotes) {
-        database.collection("image").doc(iid).set({
+        database.collection("image").doc(iid.toString()).set({
             iid: iid,
             name: name,
             sentiment: sentiment,
@@ -163,26 +226,21 @@ export function writeImageData(iid, name, sentiment, url, numVotes) {
 }
 
 export function initApp() {
-    writeEntityInStoryData(1, "s1", "test", 2, 0);
-    writeEntityInStoryData(1, "s1", "test1", 11, 0);
-    writeEntityInStoryData(1, "s1", "test1", 111, 0);
-
     var i = 1;
+    var d = new Date();
     for(i = 1; i <= 20; i++){
         writeUserData(i,'test'+i+'@test.com', 1);
         if(i <= 10){
-            var d = new Date();
-            writeStoryData(i, 's'+i, d, 'I ate McDonald\'s this morning!', 0, false);
-            writeEntityInStoryData(i, 's'+i, 'McDonald\'s', 1, 0);
+            writeStoryData(i, 1000+i, d, 'I ate McDonald\'s this morning!', 0, false);
+            writeEntityInStoryData(i, 1000+i, 'McDonald\'s', 1, 0);
             writeEntityData('McDonald\'s');
-            writeImageData('i'+i, 'McDonald\'s', 1, ['https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png'], 0);
+            writeImageData(1000000+i, 'McDonald\'s', 1, ['https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png'], 0);
         }
         else{
-            var d = new Date();
-            writeStoryData(i, 's'+i, d, 'I drank Coca Cola this evening!', 0, false);
-            writeEntityInStoryData(i, 's'+i, 'Coca Cola', 1, 0);
+            writeStoryData(i, 1000+i, d, 'I drank Coca Cola this evening!', 0, false);
+            writeEntityInStoryData(i, 1000+i, 'Coca Cola', 1, 0);
             writeEntityData('Coca Cola');
-            writeImageData('i'+i, 'Coca Cola', 1, ['https://tse2.mm.bing.net/th?id=OIP.0_ezIcFekTq93JbSIhNVNQErDQ&pid=15.1'], 0);
+            writeImageData(1000000+i, 'Coca Cola', 1, ['https://tse2.mm.bing.net/th?id=OIP.0_ezIcFekTq93JbSIhNVNQErDQ&pid=15.1'], 0);
         }
     }
 }
